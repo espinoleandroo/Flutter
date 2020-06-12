c0d0c47da1059b9ee7d7d580fbd40f21892389b3
Leandro Espino <espinoleandroo@gmail.com>
2020-06-12 16:36:33 -0500

    Curso CodigoFacilito

---
+++ flutter_app_03/lib/src/pages/listview_page.dart
@@ -0,0 +1,107 @@
+import 'dart:async';
+
+import 'package:flutter/animation.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ListaPage extends StatefulWidget {
+  @override
+  _ListaPageState createState() => _ListaPageState();
+}
+
+class _ListaPageState extends State<ListaPage> {
+
+  ScrollController _scrollController = new ScrollController();
+
+  List<int> _listaNumeros = new List();
+  int _ultimoItem = 0;
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _agregar10();
+
+    _scrollController.addListener((){
+        if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent){
+          //_agregar10();
+          fetchData();
+        }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Listas'),
+      ),
+      body: Stack(
+        children: <Widget>[
+          _crearLista(),
+          _crearLoading(),
+        ],
+      )
+    );
+  }
+
+  Widget _crearLista(){
+    return ListView.builder(
+        controller: _scrollController,
+        itemCount: _listaNumeros.length,
+        itemBuilder: (BuildContext context, int index){
+          final imagen = _listaNumeros[index];
+          return FadeInImage(
+            image: NetworkImage('https://i.picsum.photos/id/$imagen/500/300.jpg'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+          );
+        }
+    );
+  }
+  void _agregar10(){
+    for (var i = 1; i <= 10; i++){
+      _ultimoItem++;
+      _listaNumeros.add(_ultimoItem);
+    }
+    setState(() {});
+  }
+
+  Future<Null> fetchData() async{
+    _isLoading = true;
+    setState(() {});
+
+    final duracion = new Duration(seconds: 2);
+    new Timer(duracion, respuestaHTTP);
+  }
+
+  void respuestaHTTP(){
+    _isLoading = false;
+    _agregar10();
+  }
+
+  Widget _crearLoading(){
+    if(_isLoading){
+      return Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircularProgressIndicator(),
+            ],
+          ),
+          SizedBox(height: 15.0)
+        ],
+      );
+    }else{
+      return Container();
+    }
+  }
+}
--- flutter_app_03/lib/src/routes/routes.dart
+++ flutter_app_03/lib/src/routes/routes.dart
@@ -5,6 +5,7 @@ import 'package:flutterapp03/src/pages/avatar_page.dart';
 import 'package:flutterapp03/src/pages/card_page.dart';
 import 'package:flutterapp03/src/pages/home_page.dart';
 import 'package:flutterapp03/src/pages/input_page.dart';
+import 'package:flutterapp03/src/pages/listview_page.dart';
 import 'package:flutterapp03/src/pages/slider_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRouters(){
@@ -15,7 +16,8 @@ Map<String, WidgetBuilder> getApplicationRouters(){
     'card'                : (BuildContext context) => CardPage(),
     'animatedContainer'   : (BuildContext context) => AnimatedContainerPage(),
     'inputs'              : (BuildContext context) => InputPage(),
-    'slider'                : (BuildContext context) => SliderPage(),
+    'slider'              : (BuildContext context) => SliderPage(),
+    'list'                : (BuildContext context) => ListaPage(),
 
   };
 }

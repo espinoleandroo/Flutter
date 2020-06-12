49dcefc6692c16a262b794e15f31e6c2b3a202f7
Leandro Espino <espinoleandroo@gmail.com>
2020-06-12 17:22:11 -0500

    Ciclo de vida

--- flutter_app_04/lib/main.dart
+++ flutter_app_04/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
-    home: MyApp();
+    home: MyApp()
   ));
 }
 
@@ -13,6 +13,15 @@ class MyApp extends StatefulWidget{
 }
 
 class _State  extends State<MyApp>{
+
+  String _dato = 'hola mundo';
+
+  void _myClick(String dato){
+    setState(() {
+      _dato = dato;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,8 +29,27 @@ class _State  extends State<MyApp>{
         title: Text('My app'),
       ),
       body: Container(
-        child: Center(
-          child: Text('Hola mundo!'),
+        padding:  EdgeInsets.all(28.0),
+        child: Column(
+          children: <Widget>[
+            Text(_dato),
+            RaisedButton(
+              onPressed: () => _myClick('Raised'),
+              child: Text('Change texts'),
+            ),
+            FlatButton(
+              onPressed: () => _myClick('Flat'),
+              child: Text('Flat button'),
+            ),
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () => _myClick('Icon')
+            ),
+            Tooltip(
+              message: 'esto es un tooltip',
+              child: Text('Presioname'),
+            )
+          ],
         ),
       ),
     );

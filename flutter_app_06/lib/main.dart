24c6946a9272f80b0b580c1ec0764e1cabe8c2fb
Leandro Espino <espinoleandroo@gmail.com>
2020-06-12 18:37:41 -0500

    RadioButton

--- flutter_app_06/lib/main.dart
+++ flutter_app_06/lib/main.dart
@@ -15,6 +15,8 @@ class MyApp extends StatefulWidget{
 class _State  extends State<MyApp>{
   bool _isChecked = false;
   bool _value = false;
+  bool _valueRB = false;
+
   
   void onChangedCheckBox(bool Value){
     setState(() {
@@ -33,6 +35,11 @@ class _State  extends State<MyApp>{
     }else{
       print('check es false');
     }
+    if(_value){
+      print('value es true');
+    }else{
+      print('value es false');
+    }
   }
   
   @override
@@ -46,6 +53,10 @@ class _State  extends State<MyApp>{
         child: Center(
           child: Column(
             children: <Widget>[
+              Divider(
+                height: 5.0,
+                color: Colors.red,
+              ),
               Text('Checkbox'),
               Checkbox(
                 value: _isChecked,
@@ -55,11 +66,51 @@ class _State  extends State<MyApp>{
                 onPressed: getValue,
                 child: Text('Click'),
               ),
-              Text('switch'),
+              Divider(
+                height: 5.0,
+                color: Colors.red,
+              ),
+              Text('Switch'),
               Switch(
                 value: _value,
                 onChanged: (bool value){onChangedSwitch(value);},
+                activeColor: Colors.green,
+                inactiveTrackColor: Colors.red,
+              ),
+              RaisedButton(
+                onPressed: getValue,
+                child: Text('Click'),
+              ),
+              Divider(
+                height: 5.0,
+                color: Colors.red,
+              ),
+              Text('Radio Button'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Radio(
+                    value: 1,
+                    groupValue: _value,
+                    onChanged: null,
+                  ),
+                  Text('1'),
+                  Radio(
+                    value: 2,
+                    groupValue: _value,
+                    onChanged: null,
+                  ),
+                  Text('2'),
+                  Radio(
+                    value: 3,
+                    groupValue: _value,
+                    onChanged: null,
+                  ),
+                  Text('3'),
+                ],
               )
+
+
             ],
           )
         ),

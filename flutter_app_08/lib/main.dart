c131727f6bde80110daa27d2cc31b96e3a7b6a21
Leandro Espino <espinoleandroo@gmail.com>
2020-06-12 21:06:25 -0500

    Snack bar

---
+++ flutter_app_08/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: MyApp()
+  ));
+}
+
+class MyApp extends StatefulWidget{
+  @override
+  _State createState() => _State();
+
+}
+
+class _State  extends State<MyApp>{
+  final GlobalKey<ScaffoldState> _scaffold = GlobalKey<ScaffoldState>();
+  
+  void _showSnack(){
+    _scaffold.currentState.showSnackBar(SnackBar(content: Text('Esto es un Snack'),));
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffold,
+      appBar: AppBar(
+        title: Text('My app'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(28.0),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              RaisedButton(
+                onPressed: _showSnack,
+                child: Text('Click')
+              )
+            ]
+          )
+        ),
+      ),
+    );
+  }
+
+}
